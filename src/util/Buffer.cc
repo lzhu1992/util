e0e9a5486b944af4b10ef99e71e7d565c64b59b3
lzhu1992 <971796934@qq.com>
2017-06-02 14:49:59 -0400

    Update Buffer.cc

--- src/util/Buffer.cc
+++ src/util/Buffer.cc
@@ -28,19 +28,7 @@ public:
     void writemeta() {
     }
 	    
-    void Buffer(int initialSize) {
-        data = new char[initialSize];
-        data[0] = 0;
-        size = initialSize;
-        length = 0;
-        typedef char int8_t;
-        typedef signed char int8_t;
-        typedef short int16_t;
-        typedef int   int32_t;
-        typedef unsigned char     uint8_t;
-        typedef unsigned short    uint16_t;
-        typedef unsigned int      uint32_t;
-    }
+  
     //Buffer(int initialSize, const char * initialSize) {
         
     }//
